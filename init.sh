9e59acd8c93a750a7250f6a72cb34319fd7a811c
guoyaqi <1664216915@qq.com>
2018-03-05 11:35:07 +0800

    删除无用代码

--- init.sh
+++ init.sh
@@ -1,21 +1,5 @@
 #!/bin/sh
-# USER='qiqi'
-# echo ${USER:1}
 
-# name=$1
-# firstLetter=${name:0:1}
-# uppercaseFirstLetter="$(echo $firstLetter | tr '[:lower:]' '[:upper:]')"
-# filename="my$uppercaseFirstLetter${name:1}.js"
-# testFilename="my$uppercaseFirstLetter${name:1}.test.js"
-#
-# mkdir $name
-# echo $name '文件夹已创建'
-# touch $name/$filename
-# echo $filename '文件已创建'
-# touch $name/$testFilename
-# echo $testFilename '文件已创建'
-# touch $name/'README.md'
-# echo 'README.md文件已创建'
 directory=$1
 name="my$(tr '[:lower:]' '[:upper:]' <<< ${directory:0:1})${directory:1}.js"
 testName="my$(tr '[:lower:]' '[:upper:]' <<< ${directory:0:1})${directory:1}.test.js"
